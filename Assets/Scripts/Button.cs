cf23bd5c5c1d2c0fdbda4ab1867b41f90e71caea
jovi <jovitej@gmail.com>
2020-08-06 20:18:32 +0800

    added small drink stall stuff

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Button : MonoBehaviour
+{
+    private PlayerController pc;
+    private TextMeshPro tmp;
+
+    private void Awake()
+    {
+        pc = FindObjectOfType<PlayerController>();
+        tmp = GetComponentInChildren<TextMeshPro>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmp.GetComponent<MeshRenderer>().sortingOrder = GetComponent<SpriteRenderer>().sortingOrder;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0.0f;
+    }
+
+    void OnMouseEnter()
+    {
+        Debug.Log("I am over something");
+    }
+
+    void OnMouseUpAsButton()
+    {
+        Toolbox.Instance.Get<GameController>().Print(this.name + " is pressed");
+    }
+
+
+    void OnMouseExit()
+    {
+        Debug.Log("I have left something");
+    }
+}
