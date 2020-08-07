3c7ccd1c6779704dfe1690ab29daeaf794ef7a09
Jovi <jovitej@gmail.com>
2020-08-07 16:18:29 +0800

    Added metric scene.

---
+++ Assets/meter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meter : MonoBehaviour
+{
+    private bool isCoroutineExecuting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isCoroutineExecuting = false;
+        float originalRotation = 75.0f;
+        var seq = LeanTween.sequence();
+        seq.append(LeanTween.rotateZ(this.gameObject, originalRotation, 4.5f).setDelay(2.0f).setEaseInOutBounce());
+        Toolbox.Instance.Sfx.PlaySound("drumroll_0", 0.25f);
+        StartCoroutine(ExecuteAfterTime(4.5f));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        PlayLousyHumming();
+    }
+
+    void PlayLousyHumming()
+    {
+        Toolbox.Instance.Sfx.PlaySound("lousy_humming_0", 0.25f);
+    }
+}
